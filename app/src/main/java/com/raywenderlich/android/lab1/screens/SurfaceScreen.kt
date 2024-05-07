c96bd63de01ea00516c5157bf762fa8a2a82704e
temychshev <artshchnk@gmail.com>
2024-05-07 18:59:01 +0300

    Create SurfaceScreen

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/SurfaceScreen.kt
@@ -0,0 +1,27 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+
+@Composable
+fun SurfaceScreen(modifier: Modifier = Modifier) {
+
+    Box(modifier = modifier.fillMaxSize()) {
+        MySurface(modifier= modifier.align(Alignment.Center))
+    }
+
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MySurface(modifier: Modifier) {
+//TODO write your code here
+}
