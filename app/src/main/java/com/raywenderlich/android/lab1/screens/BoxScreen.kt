ca5739ffea67690c17eca9bec1ab7eee60580801
temychshev <artshchnk@gmail.com>
2024-05-07 19:00:20 +0300

    Create BoxScreen

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/BoxScreen.kt
@@ -0,0 +1,21 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+
+@Composable
+fun BoxScreen() {
+    MyBox()
+
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MyBox(modifier: Modifier = Modifier) {
+    //TODO write your code here
+}
